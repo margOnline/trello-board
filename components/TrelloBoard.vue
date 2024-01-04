49bf53ca6d01d0551be95359b6827e63fd519930
Margo Urey <margo@margonline.co.uk>
2024-01-04 17:21:58 +0000

    Facility to delete a column by using the backspace to edit the column title to an empty string

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -79,6 +79,11 @@ function createColumn(){
             <input
               class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
               @keyup.enter="($event.target as HTMLInputElement).blur()"
+              @keydown.backspace="
+                column.title === ''
+                  ? (columns = columns.filter((c) => c.id !== column.id))
+                  : null
+              "
               type="text"
               v-model="column.title"
             />
